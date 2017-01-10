d014b6b7d8952122ba65f8c096bea53ca8d71cff
tangert <tyler@angert.com>
2017-01-10 16:17:11 -0500

    Initial Commit

---
+++ speech2caps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  speech2caps
+//
+//  Created by Tyler Angert on 1/10/17.
+//  Copyright © 2017 Tyler Angert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
